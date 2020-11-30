32ecd66d973d5c6f54425c64a52ce338a278b45d
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-11-30 12:45:47 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 
@@ -53,6 +54,7 @@ import { LoginPageComponent } from "./login-page/login-page.component";
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     A11yModule,
     ClipboardModule,
