6e2d4b1a0ff389a075a706f918538727ec4fdc7d
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-02 18:40:41 +0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { LoginPageComponent } from "./login-page/login-page.component";
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "login", component: LoginPageComponent },
-  { path: "dashboard", component: DashboardPageComponent }
+  { path: "dashboard", component: DashboardPageComponent },
   { path: "register", component: RegistrationPageComponent }
   // { path: "admin", component: AdminPageComponent },
   // { path: "admin/:adminCommand", component: AdminPageComponent }
