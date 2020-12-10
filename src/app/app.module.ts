3432359c81681fd3f2a3f7281c22a38fed4f2488
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-10 17:22:01 +0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,7 +61,7 @@ import { ProfilPageComponent } from "./profil-page/profil-page.component";
 import { MemberPageComponent } from "./member-page/member-page.component";
 import { MenuBarService } from "./shared/menu-bar.service";
 import { AknutmanWsService } from "./shared/aknutman-ws.service";
-import { TermConditionDialogComponent } from './term-condition-dialog/term-condition-dialog.component';
+import { TermConditionDialogComponent } from "./term-condition-dialog/term-condition-dialog.component";
 
 @NgModule({
   imports: [
@@ -129,7 +129,10 @@ import { TermConditionDialogComponent } from './term-condition-dialog/term-condi
     TermConditionDialogComponent
   ],
   bootstrap: [AppComponent],
-  entryComponents: [LoginWarningSnackbarComponent],
+  entryComponents: [
+    LoginWarningSnackbarComponent,
+    TermConditionDialogComponent
+  ],
   providers: [MenuBarService, AknutmanWsService]
 })
 export class AppModule {}
