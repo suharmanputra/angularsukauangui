71a2f0c1ff600b96b48edc1021a9f9236be7e5ca
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-10 17:06:14 +0700

    2 files modified

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { MenuBarService } from "../shared/menu-bar.service";
 import { ActivatedRoute, Router, RoutesRecognized } from "@angular/router";
-import { TermConditionDialogComponent } from "./term-condition-dialog/term-condition-dialog.component";
+import { MatDialog } from "@angular/material/dialog";
+import { TermConditionDialogComponent } from "../term-condition-dialog/term-condition-dialog.component";
 @Component({
   selector: "app-dashboard-page",
   templateUrl: "./dashboard-page.component.html",
@@ -11,7 +12,7 @@ export class DashboardPageComponent implements OnInit {
   constructor(
     private menuBarService: MenuBarService,
     private router: Router,
-    public dialog: TermConditionDialogComponent
+    public dialog: MatDialog
   ) {}
 
   ngOnInit() {
