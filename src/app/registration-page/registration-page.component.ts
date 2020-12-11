19d778dd64e5fc5a7ba417d9b0382dfa0e694366
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-11 23:55:56 +0700

    2 files modified

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { MenuBarService } from "../shared/menu-bar.service";
 import { ActivatedRoute, Router, RoutesRecognized } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+
 @Component({
   selector: "app-registration-page",
   templateUrl: "./registration-page.component.html",
   styleUrls: ["./registration-page.component.css"]
 })
 export class RegistrationPageComponent implements OnInit {
+  // txtReff = this.refftext;
   constructor(
     private menuBarService: MenuBarService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute // private refftext: string
   ) {}
 
   ngOnInit() {
@@ -25,9 +28,12 @@ export class RegistrationPageComponent implements OnInit {
       if (params["reff"] != null) {
         // this.txtReferal = referalCode;
         console.log(params["reff"]);
+        this.txtReff = params["reff"];
       } else {
         // this.txtReferal = "SUKAUANG";
-        console.log("SUKAUANG");
+        // console.log("SUKAUANG");
+        // txtReff = "SUKAUANG";
+        this.txtReff = "SUKAUANG";
       }
     });
 
