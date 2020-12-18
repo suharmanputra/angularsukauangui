42ddfd46f46792158e2b03fbf6823bcb64619153
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-18 21:39:10 +0700

    1 file modified

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { AknutmanWsService } from "../shared/aknutman-ws.service";
 import { MenuBarService } from "../shared/menu-bar.service";
 import { ActivatedRoute, Router, RoutesRecognized } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
 @Component({
   selector: "app-admin-page",
   templateUrl: "./admin-page.component.html",
@@ -12,6 +13,7 @@ export class AdminPageComponent implements OnInit {
   constructor(
     private aknutman: AknutmanWsService,
     private menuBarService: MenuBarService,
+    private snackBar: MatSnackBar,
     private router: Router
   ) {}
 
@@ -32,11 +34,36 @@ export class AdminPageComponent implements OnInit {
     // });
   }
   tampildata(datefrom: string, dateto: string) {
-    console.log(datefrom, dateto);
-    // this.menuBarService.setLoadingAnimation(true);
-    // this.aknutman.getuserlist("2020-12-01", "2020-12-18").subscribe(resp => {
-    //   this.listdatauser = resp.persons;
-    //   this.menuBarService.setLoadingAnimation(false);
-    // });
+    if (datefrom == "") {
+      this.snackBar.open("Pilih tanggal awal terlebih dahulu!", "Ok", {
+        duration: 3000
+      });
+    } else if (dateto == "") {
+      this.snackBar.open("Pilih tanggal akhir terlebih dahulu!", "Ok", {
+        duration: 3000
+      });
+    } else {
+      this.menuBarService.setLoadingAnimation(true);
+      this.aknutman
+        .getuserlist(this.formatDate(datefrom), this.formatDate(dateto))
+        .subscribe(resp => {
+          this.listdatauser = resp.persons;
+          this.menuBarService.setLoadingAnimation(false);
+        });
+    }
+
+    // console.log(this.formatDate(datefrom), this.formatDate(dateto));
+  }
+
+  formatDate(date: string) {
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
   }
 }
