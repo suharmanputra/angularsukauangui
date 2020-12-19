f4f70a263c56630f16986414bf15032950340f9d
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-19 11:32:20 +0700

    2 files modified

--- src/app/transaksi-page/transaksi-page.component.ts
+++ src/app/transaksi-page/transaksi-page.component.ts
@@ -67,18 +67,6 @@ export class TransaksiPageComponent implements OnInit {
     });
   }
 
-  formatDate(date: string) {
-    var d = new Date(date),
-      month = "" + (d.getMonth() + 1),
-      day = "" + d.getDate(),
-      year = d.getFullYear();
-
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
-
-    return [year, month, day].join("-");
-  }
-
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
