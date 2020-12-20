af1e4d213cba406afb7eae6fad01defcfbc4144c
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-20 20:48:47 +0700

    2 files modified

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -73,13 +73,13 @@ export class DashboardPageComponent implements OnInit {
               this.statusakun = "Menunggu Konfirmasi Admin";
               this.aktivasiButtonVisible = false;
               this.buktitrfButtonVisible = true;
-              this.buktitrffile =
-                `<a target="_blank"
-									href="` +
-                respdetailuser.data.PaymentProofStorage +
-                `"><img style="display:inline-block;width:50px;height:50px;" src="` +
-                respdetailuser.data.PaymentProofStorage +
-                `"></a>`;
+              this.buktitrffile = respdetailuser.data.PaymentProofStorage;
+              //   `<a target="_blank"
+              // 		href="` +
+              //   respdetailuser.data.PaymentProofStorage +
+              //   `"><img style="display:inline-block;width:50px;height:50px;" src="` +
+              //   respdetailuser.data.PaymentProofStorage +
+              //   `"></a>`;
             }
             this.masaaktif = "0";
             this.checkinButtonVisible = false;
