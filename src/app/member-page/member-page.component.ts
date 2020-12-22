2cb5536cd5d13c9356dd3c83820cf7c7a1f5829e
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-22 22:11:50 +0700

    4 files modified

--- src/app/member-page/member-page.component.ts
+++ src/app/member-page/member-page.component.ts
@@ -7,6 +7,38 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ["./member-page.component.css"]
 })
 export class MemberPageComponent implements OnInit {
+  member1: string;
+  member2: string;
+  member3: string;
+  member4: string;
+  member5: string;
+  member6: string;
+  member7: string;
+  member8: string;
+  member9: string;
+  member10: string;
+  member11: string;
+  member12: string;
+  member13: string;
+  member14: string;
+  member15: string;
+  member16: string;
+  member17: string;
+  member18: string;
+  member19: string;
+  member20: string;
+  member21: string;
+  member22: string;
+  member23: string;
+  member24: string;
+  member25: string;
+  member26: string;
+  member27: string;
+  member28: string;
+  member29: string;
+  member30: string;
+  member31: string;
+
   constructor(private menuBarService: MenuBarService, private router: Router) {}
 
   ngOnInit() {
