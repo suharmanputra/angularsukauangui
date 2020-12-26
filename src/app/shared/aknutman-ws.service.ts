1610f76dcec2d31cb4623c16d7ea287cba1440ee
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2020-12-26 21:51:58 +0700

    3 files modified

--- src/app/shared/aknutman-ws.service.ts
+++ src/app/shared/aknutman-ws.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpEvent } from "@angular/common/http";
 
-// export interface wsResponseType {
-//   Status: string;
-//   IsAuthenticated: boolean;
-// }
-
 @Injectable()
 export class AknutmanWsService {
   constructor(private http: HttpClient) {}
